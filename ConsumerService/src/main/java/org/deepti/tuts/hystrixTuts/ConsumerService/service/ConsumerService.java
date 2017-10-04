e7cb35b1029b955aa2a7a4a6951dc1f58088f1e1
Deepti Mittal <deepi2003@gmail.com>
2017-10-04 21:06:32 +0530

    Add threadpool properties and read the properties back

--- ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/service/ConsumerService.java
+++ ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/service/ConsumerService.java
@@ -1,9 +1,14 @@
 package org.deepti.tuts.hystrixTuts.ConsumerService.service;
 
 
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandMetrics;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolMetrics;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,9 +21,19 @@ public class ConsumerService {
     private RestTemplate restTemplate;
 
     //commandProperties = {@HystrixProperty(name = "requestCache.enabled", value = "false")},
-    @HystrixCommand(fallbackMethod = "fallBackGetHello",
-            commandProperties = {@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "1")})
+    @HystrixCommand(
+            fallbackMethod = "fallBackGetHello",
+            commandKey = "test",
+            threadPoolKey = "test",
+            commandProperties = {
+                    @HystrixProperty(name = "requestCache.enabled", value = "false")
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "30")
+            })
     public String getHello() {
+        System.out.println(" COmmand metrics----------------  "+ HystrixCommandMetrics.getInstance(getCommandKey()).getProperties().requestCacheEnabled().get());
+        System.out.println(" thread metrics----------------  "+ HystrixThreadPoolMetrics.getInstance(getThreadPoolKey()).getProperties().coreSize().get());
         URI uri = URI.create("http://localhost:5678/");
         return restTemplate.getForObject(uri, String.class);
 
@@ -27,4 +42,22 @@ public class ConsumerService {
     private String fallBackGetHello() {
         return "Fallback hello";
     }
+
+    public String getGoogle() {
+        String result;
+        String httpResult = restTemplate.getForObject("http://google.com",
+                String.class);
+        result = "Message SUCCESS result: " + httpResult;
+
+        return result;
+    }
+
+    private static HystrixCommandKey getCommandKey() {
+        return HystrixCommandKey.Factory.asKey("test");
+    }
+
+    private static HystrixThreadPoolKey getThreadPoolKey() {
+        return HystrixThreadPoolKey.Factory.asKey("test");
+    }
+
 }
