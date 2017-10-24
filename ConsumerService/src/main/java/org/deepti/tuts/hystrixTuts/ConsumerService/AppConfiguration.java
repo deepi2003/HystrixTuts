46606a2afb41b1bb703d0acb6a55765332e3f453
Deepti Mittal <deepi2003@gmail.com>
2017-10-24 21:02:03 +0530

    Using spring cloud hystrix to configure hystrix properties in application.yml

--- ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/AppConfiguration.java
+++ ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/AppConfiguration.java
@@ -1,7 +1,6 @@
 package org.deepti.tuts.hystrixTuts.ConsumerService;
 
 
-import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration {
 
-    @Bean
-    public HystrixCommandAspect hystrixAspect() {
-        return new HystrixCommandAspect();
-    }
 
     @Bean
     public ServletRegistrationBean hystrixStreamServlet(){
--- ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/Application.java
+++ ConsumerService/src/main/java/org/deepti/tuts/hystrixTuts/ConsumerService/Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@EnableCircuitBreaker
 public class Application {
 
     @Autowired
